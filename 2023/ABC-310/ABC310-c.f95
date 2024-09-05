18ba5f25e9b7e98adb0f65b29f43e860ed2839c3
onodera-g <ga19981023ku@gmail.com>
2024-09-05 11:31:51 +0000

    ABC310

--- 2023/ABC-310/ABC310-c.f95
+++ 2023/ABC-310/ABC310-c.f95
@@ -1,55 +1,107 @@
 program ABC310c
+    use stdlib_kinds
+    use stdlib_hashmaps
+    use stdlib_hashmap_wrappers
     implicit none
-    integer(8) i, j, k, check, cnt
-    integer(8) N, tmp
-    character(10), allocatable :: S(:)
-    character(10) tmp1, tmp2
 
-    !初期化
-    i = 0; tmp = 0; cnt = 0
-    !入力
+    !hashmap
+    type(key_type)              :: key
+    type(other_type)            :: other
+    type(chaining_hashmap_type) :: map
+    type dummy_type
+        integer                 :: value(1)
+    end type dummy_type
+    type(dummy_type)            :: dummy
+    class(*), allocatable       :: data
+
+    !変数
+    integer i, j, k
+    integer N, cnt, lenA, MAXlen_A, A1(122)
+    character(1) mozi
+    character(100000) A, A_reverse
+    logical present, flag_cnt
+
+    !hashmapの初期化
+    call map%init(fnv_1_hasher)
+    dummy%value = 1
+    call set(other, dummy)
+
+    !異なる棒の種類数
     read (*, *) N
-    allocate (S(N))
+    cnt = 0; A1 = 0
     do i = 1, N
-        read (*, *) S(i)
+        read (*, *) A
+        if (len_trim(A) == 1) then !１文字の場合
+            mozi = trim(A)
+            A1(ichar(mozi)) = A1(ichar(mozi)) + 1
+        else !２文字以上の場合
+            flag_cnt = .false.
+            key%value = transfer(trim(A), key%value)
+            call map%key_test(key, present)
+            !Aが登録済みかチェック
+            if (present .eqv. .false.) then !無
+                call map%map_entry(key, other)
+                flag_cnt = .true.
+            end if
+            !Aの逆順が登録済みかチェック
+            lenA = len_trim(A)
+            if (lenA /= 1) then
+                A_reverse = A
+                call reverse_string(A_reverse(1:lenA))
+                key%value = transfer(trim(A_reverse), key%value)
+                call map%key_test(key, present)
+                if (present .eqv. .false.) then !無
+                    call map%map_entry(key, other)
+                    flag_cnt = .true.
+                end if
+            end if
+            !未登録ならカウント
+            if (flag_cnt .eqv. .true.) then
+                cnt = cnt + 1
+                !print *, trim(A)
+            end if
+        end if
+    end do
+    !１文字の分のカウント
+    do i = 97, 122
+        if (A1(i) > 0) then
+            cnt = cnt + 1
+            !print *, char(i)
+        end if
     end do
 
-    !前後判定の検証
-    do i = 1, N
-        !write (*, '(i0,4x*(a))') i, S(i)
-        do j = 1, N
-            !write (*, '(4x,i0,4x*(a))') j, S(j)
-            !例外を弾く
-            if (i == j) cycle
-            if (len_trim(S(i)) /= len_trim(S(j))) cycle
-            !比較:回文
-            check = 1
-            tmp1 = S(i)
-            tmp2 = adjustr(S(j))
-            do k = 1, len_trim(S(i))
-                if (tmp1(k:k) /= tmp2(11 - k:11 - k)) then
-                    check = 0
-                    exit
-                end if
-            end do
-            if (check == 1) then
-                write (*, *) i, j
-                cnt = cnt + 1
-            end if
-            !比較:完全一致
-            check = 1
-            tmp2 = S(j)
-            do k = 1, len_trim(S(i))
-                if (tmp1(k:k) /= tmp2(k:k)) then
-                    check = 0
-                    exit
-                end if
-            end do
-            if (check == 1) then
-                write (*, *) i, j
-                cnt = cnt + 1
-            end if
+    !結果の出力
+    print *, cnt
+contains
+
+    subroutine reverse_string(original)
+        implicit none
+        character(len=*), intent(inout) :: original
+        character(len=:), allocatable :: temp_string
+        integer :: i, length_of_string
+
+        ! 変数の説明:
+        ! original: 逆さまにする元の文字列（入力兼出力）
+        ! temp_string: 逆さまにした一時的な文字列
+        ! length_of_string: 元の文字列の長さ
+        ! i: ループカウンタ
+
+        ! 元の文字列の長さを取得
+        length_of_string = len_trim(original)
+
+        ! 一時的な文字列temp_stringにメモリを確保
+        allocate (character(len=length_of_string) :: temp_string)
+
+        ! 文字列を逆さまにする処理
+        do i = 1, length_of_string
+            temp_string(i:i) = original(length_of_string - i + 1:length_of_string - i + 1)
         end do
-    end do
-    write (*, *) cnt
-end
+
+        ! 結果をoriginalに上書き
+        original = temp_string
+
+        ! メモリを解放
+        deallocate (temp_string)
+
+    end subroutine reverse_string
+end program ABC310c

f1f9a7aedca15106ee98c8b67784b5e8c0f14fd3
onodera-g <ga19981023ku@gmail.com>
2025-06-07 15:45:58 +0000

    ABC409

--- 2025/ABC409/ABC409c.f95
+++ 2025/ABC409/ABC409c.f95
@@ -7,7 +7,7 @@ program ABC409c
     ! freq(p)   ：位置 p に存在する点の個数（添字範囲 0..L-1）
     ! cnt       ：正三角形を構成する点の組み合わせ数（64ビット整数）
 
-    use, intrinsic :: iso_fortran_env, only: int32, int64
+    use, intrinsic :: iso_fortran_env, only: int64, int64
     implicit none
 
     integer(int64) :: N, L
@@ -15,33 +15,33 @@ program ABC409c
     integer(int64) :: i, p, q, r, delta
     integer(int64) :: cnt
 
-    !--- 入力読み込み ---
+    ! 入力
     read (*, *) N, L
     allocate (d(N - 1))
     read (*, *) (d(i), i=1, N - 1)
 
-    !--- 点の位置を累積和で計算 ---
+    ! 点の位置を累積和で計算
     allocate (pos(N))
-    pos(1) = 0_int32
+    pos(1) = 0_int64
     do i = 2, N
         pos(i) = mod(pos(i - 1) + d(i - 1), L)
     end do
 
-    !--- L が 3 の倍数でなければ正三角形は存在しない ---
-    if (mod(L, 3_int32) /= 0_int32) then
+    ! L が 3 の倍数でなければ正三角形は存在しない
+    if (mod(L, 3_int64) /= 0_int64) then
         print *, 0_int64
         stop
     end if
-    delta = L/3_int32
+    delta = L/3_int64
 
-    !--- 各位置にいる点の個数をカウント (添字 0..L-1) ---
+    ! 各位置にいる点の個数をカウント (添字 0..L-1)
     allocate (freq(0:L - 1))
     freq = 0
     do i = 1, N
         freq(pos(i)) = freq(pos(i)) + 1
     end do
 
-    !--- 正三角形を構成する組み合わせ数を計算 ---
+    ! 正三角形を構成する組み合わせ数を計算
     cnt = 0_int64
     do p = 0, L - 1
         if (freq(p) == 0) cycle
@@ -52,10 +52,8 @@ program ABC409c
         end if
     end do
 
-    ! 頂点起点を3度数えているので3で割って実際の個数に
+    ! 結果の出力
     cnt = cnt/3_int64
-
-    !--- 結果出力 ---
     print *, cnt
 
 end program ABC409c
